a6dd328d03a31b2852359f68796bf6922c0a5ece
alx <alex@MCs-iMac-2.local>
2016-01-08 08:16:06 +0000

    Added new metronome (unimplemented)

---
+++ Bassline_Explorer/js/lib/metronome/metronome.js
@@ -0,0 +1,102 @@
+var audioContext = null;
+var isPlaying = false;			// Are we currently playing?
+var startTime;					// The start time of the entire sequence.
+var current16thNote;			// What note is currently last scheduled?
+var tempo = 120.0;				// tempo (in beats per minute)
+var lookahead = 25.0;			// How frequently to call scheduling function
+								//(in milliseconds)
+var scheduleAheadTime = 0.1;	// How far ahead to schedule audio (sec)
+								// This is calculated from lookahead, and overlaps
+								// with next interval (in case the timer is late)
+var nextNoteTime = 0.0;			// when the next note is due.
+var noteResolution = 0;			// 0 == 16th, 1 == 8th, 2 == quarter note
+var noteLength = 0.05;			// length of "beep" (in seconds)
+var notesInQueue = [];			// the notes that have been put into the web audio,
+								// and may or may not have played yet. {note, time}
+var timerWorker = null;			// The Web Worker used to fire timer messages
+
+function nextNote() {
+    // Advance current note and time by a 16th note...
+    var secondsPerBeat = 60.0 / tempo;		// Notice this picks up the CURRENT
+    										// tempo value to calculate beat length.
+    nextNoteTime += 0.25 * secondsPerBeat;	// Add beat length to last beat time
+
+    current16thNote++;						// Advance the beat number, wrap to zero
+    if (current16thNote == 16) {
+        current16thNote = 0;
+    }
+}
+
+function scheduleNote( beatNumber, time ) {
+    // Push the note on the queue, even if we're not playing.
+    notesInQueue.push( { note: beatNumber, time: time } );
+
+    if ( (noteResolution==1) && (beatNumber%2))
+        return; // We're not playing non-8th 16th notes
+    if ( (noteResolution==2) && (beatNumber%4))
+        return; // We're not playing non-quarter 8th notes
+
+    // Create an oscillator
+    var osc = audioContext.createOscillator();
+    osc.connect( audioContext.destination );
+    if (beatNumber % 16 === 0)		// Beat 0 == low pitch
+        osc.frequency.value = 880.0;
+    else if (beatNumber % 4 === 0 )	// Quarter notes = medium pitch
+        osc.frequency.value = 440.0;
+    else							// Other 16th notes = high pitch
+        osc.frequency.value = 220.0;
+
+    osc.start( time );
+    osc.stop( time + noteLength );
+	
+	console.log("Tick!");
+}
+
+function scheduler() {
+    // While there are notes that will need to play before the next interval, 
+    // Schedule them and advance the pointer.
+    while (nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
+        scheduleNote( current16thNote, nextNoteTime );
+        nextNote();
+    }
+}
+
+function play() {
+    isPlaying = !isPlaying;
+
+    if (isPlaying) { // Start playing
+        current16thNote = 0;
+        nextNoteTime = audioContext.currentTime;
+        timerWorker.postMessage("start");
+        return "stop";
+    } else {
+        timerWorker.postMessage("stop");
+        return "play";
+    }
+}
+
+function init(){
+    // NOTE: THIS RELIES ON THE MONKEYPATCH LIBRARY BEING LOADED FROM
+    // http://cwilso.github.io/AudioContext-MonkeyPatch/AudioContextMonkeyPatch.js
+    // TO WORK ON CURRENT CHROME!!  But this means our code can be properly
+    // spec-compliant, and work on Chrome, Safari and Firefox.
+
+    audioContext = new AudioContext();
+
+    // If we wanted to load audio files, etc., this is where we should do it.
+
+    timerWorker = new Worker("js/metronomeworker.js");
+
+    timerWorker.onmessage = function(e) {
+        if (e.data == "tick") {
+            // Console.log("tick!");
+            scheduler();
+        }
+        else
+            console.log("message: " + e.data);
+    };
+    timerWorker.postMessage({"interval":lookahead});
+}
+
+window.addEventListener("load", init );
+
